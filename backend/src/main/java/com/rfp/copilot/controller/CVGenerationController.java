45024d561cd55ca73c82336746964d9a115dd72a
Lubna(office) <lubna.haque@dsinnovators.com>
2025-05-27 15:26:18 +0600

    need to check database issue

---
+++ backend/src/main/java/com/rfp/copilot/controller/CVGenerationController.java
@@ -0,0 +1,20 @@
+package com.rfp.copilot.controller;
+
+import com.rfp.copilot.service.CVGenerationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CVGenerationController {
+    private final CVGenerationService cvGenerationService;
+
+    public CVGenerationController(CVGenerationService cvGenerationService) {
+        this.cvGenerationService = cvGenerationService;
+    }
+
+    @GetMapping("/generate-cv")
+    public String simplify () {
+
+        return cvGenerationService.generateCv();
+    }
+}
---
+++ backend/src/main/java/com/rfp/copilot/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.rfp.copilot.repository;
+
+import com.rfp.copilot.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
