5c6cb090e7bc579932cddf3e093fb91434838204
Lubna(office) <lubna.haque@dsinnovators.com>
2025-05-27 12:14:25 +0600

    Add sql files for employee table creation and employees data migrations

---
+++ backend/src/main/resources/DB sql/migrate_employees.sql
@@ -0,0 +1,29 @@
+INSERT INTO employees (name, position, academic_qualification, subject, experience, skills, certification, experience_details) VALUES
+    ('Brian Smith', 'Software Engineer', 'Masters of Science', 'ICT', 3, 'Spring boot, react, Android', 'CMMI 1', 'Coffey-Gonzalez; 2018-05-03 to 2019-11-25; Languages: Bengali; Responsibilities: Report increase politics relate garden today. Image according while day future. End its let.');
+
+INSERT INTO employees (name, position, academic_qualification, subject, experience, skills, certification, experience_details) VALUES
+    ('Brenda Richards', 'Quality Assurance', 'Bachelor of Science', 'ICT', 10, 'javascript, oracle, iOS', 'CMMI 4, CMMI 3', 'Zimmerman-Keith; 2016-12-25 to 2025-02-16; Languages: English, Hindi; Responsibilities: Degree yes meeting very president Congress society.');
+
+INSERT INTO employees (name, position, academic_qualification, subject, experience, skills, certification, experience_details) VALUES
+    ('Natalie Coleman', 'Software Engineer', 'Masters of Science', 'ICT', 1, 'DBA, mysql, Web', 'CMMI 1, CMMI 3, Scrum Master', 'Sanchez Inc; 2020-09-07 to 2022-07-23; Languages: English, Bengali; Responsibilities: Morning happen these this figure.');
+
+INSERT INTO employees (name, position, academic_qualification, subject, experience, skills, certification, experience_details) VALUES
+    ('Jeffrey Gill Jr.', 'Project Manager', 'Masters of Science', 'Software Engineer', 1, 'postgres, Java, Android', 'CMMI 4, CMMI 2', 'Joseph LLC; 2022-11-08 to 2022-12-15; Languages: Bengali; Responsibilities: Do environmental long late soon. Special ball white should agency guess. Unit girl crime arm.');
+
+INSERT INTO employees (name, position, academic_qualification, subject, experience, skills, certification, experience_details) VALUES
+    ('Jennifer Lawrence', 'Quality Assurance', 'Masters of Science', 'Software Engineer', 4, 'Spring boot, Java, ERP, next, Web', 'Scrum Master', 'Diaz-Edwards; 2015-07-08 to 2023-04-21; Languages: English; Responsibilities: Before health mention article remain in. Purpose rate simple least doctor.');
+
+INSERT INTO employees (name, position, academic_qualification, subject, experience, skills, certification, experience_details) VALUES
+    ('Kaitlyn Drake', 'Quality Assurance', 'Masters of Science', 'ICT', 6, 'react, DBA, Spring boot, ERP, iOS, postgres', 'Scrum Master, CMMI 5', 'Mcfarland-Ferguson; 2020-07-13 to 2021-06-18; Languages: Hindi, Bengali; Responsibilities: Notice impact smile rate white trade.');
+
+INSERT INTO employees (name, position, academic_qualification, subject, experience, skills, certification, experience_details) VALUES
+    ('Diana Shaw', 'Team Lead', 'Masters of Science', 'Software Engineer', 1, 'ERP, javascript, Spring boot, Java, oracle', 'CMMI 3', 'Myers Inc; 2016-10-10 to 2021-06-24; Languages: English; Responsibilities: Whose yard buy poor once low quickly. Later seek second beautiful Republican life.');
+
+INSERT INTO employees (name, position, academic_qualification, subject, experience, skills, certification, experience_details) VALUES
+    ('Thomas Smith', 'Project Manager', 'Bachelor of Science', 'Software Engineer', 14, 'mysql, DBA, ERP', 'CMMI 2, CMMI 4, CMMI 1', 'Bennett, Harris and Sanchez; 2022-12-27 to 2024-10-26; Languages: English; Responsibilities: Issue stay ground owner. Level Mr organization. Leave measure drop. Then these seat Congress.');
+
+INSERT INTO employees (name, position, academic_qualification, subject, experience, skills, certification, experience_details) VALUES
+    ('Mary Wolf', 'Project Manager', 'Masters of Science', 'Computer Science Engineer', 1, 'Spring boot, react, ERP, next', 'CMMI 1, CMMI 5, Scrum Master', 'Salazar-Hancock; 2017-06-24 to 2022-10-09; Languages: English; Responsibilities: City difficult process sport let stay discuss. Couple radio center.');
+
+INSERT INTO employees (name, position, academic_qualification, subject, experience, skills, certification, experience_details) VALUES
+    ('Tiffany King', 'Quality Assurance', 'Bachelor of Science', 'ICT', 14, 'javascript, next, mysql', 'Scrum Master, CMMI 5, CMMI 1', 'Wall-Weber; 2017-01-06 to 2021-07-11; Languages: Bengali; Responsibilities: Condition age meet challenge support kitchen have. Event situation every first.');
