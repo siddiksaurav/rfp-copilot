f39c0375dab407e982779b3b0dc00653fe58ebc5
Lubna(office) <lubna.haque@dsinnovators.com>
2025-05-27 12:14:25 +0600

    Add sql files for employee table creation and employees data migrations

---
+++ backend/src/main/resources/DB sql/create_employee.sql
@@ -0,0 +1,11 @@
+CREATE TABLE employees (
+   id SERIAL PRIMARY KEY,
+   name TEXT,
+   position TEXT,
+   academic_qualification TEXT,
+   subject TEXT,
+   experience INTEGER,
+   skills TEXT,
+   certification TEXT,
+   experience_details TEXT
+);
